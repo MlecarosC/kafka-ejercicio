af161220c2454f56a7409d2055428d4dfceae35e
Martin <martin.lecaros@3it.cl>
2025-11-17 11:18:36 -0300

    fix: propertie data

--- consumer/src/main/java/com/kafka/consumer/listener/KafkaConsumerListener.java
+++ consumer/src/main/java/com/kafka/consumer/listener/KafkaConsumerListener.java
@@ -1,13 +1,20 @@
 package com.kafka.consumer.listener;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
+@Component
 public class KafkaConsumerListener {
-    @KafkaListener(topics = "notification-topic", groupId = "notification-group")
+
+    @Value("${notification.kafka.topic}")
+    private String topic;
+
+    @KafkaListener(topics = "${notification.kafka.topic}", groupId = "notification-group")
     public void listen(String message) {
         log.info("Received Message: {}", message);
     }
