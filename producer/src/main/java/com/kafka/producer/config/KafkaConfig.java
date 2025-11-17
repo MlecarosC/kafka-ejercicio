4adae87920610975ca928bc9211dfb3ec5103e9c
Martin <martin.lecaros@3it.cl>
2025-11-17 11:48:31 -0300

    fix: inconsistent Data in producer config & enablekafka in consumer

--- producer/src/main/java/com/kafka/producer/config/KafkaConfig.java
+++ producer/src/main/java/com/kafka/producer/config/KafkaConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 
 @Configuration
@@ -17,16 +18,16 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return props;
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        DefaultKafkaProducerFactory<String, String> producerFactory =
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        DefaultKafkaProducerFactory<String, Object> producerFactory =
                 new DefaultKafkaProducerFactory<>(producerProperties());
-        KafkaTemplate<String, String> template = new KafkaTemplate<>(producerFactory);
+        KafkaTemplate<String, Object> template = new KafkaTemplate<>(producerFactory);
         return template;
     }
 }
