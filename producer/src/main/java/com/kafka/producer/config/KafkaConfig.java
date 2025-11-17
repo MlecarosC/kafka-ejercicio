19c0bb25ab332df2363766a3abeeb161f61b278b
Martin <martin.lecaros@3it.cl>
2025-11-17 10:45:27 -0300

    feat: first commit, kafka producer and consumer config added to each microservice

---
+++ producer/src/main/java/com/kafka/producer/config/KafkaConfig.java
@@ -0,0 +1,33 @@
+package com.kafka.producer.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+
+
+@Configuration
+public class KafkaConfig {
+    public Map<String, Object> producerProperties() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        DefaultKafkaProducerFactory<String, String> producerFactory =
+                new DefaultKafkaProducerFactory<>(producerProperties());
+        KafkaTemplate<String, String> template = new KafkaTemplate<>(producerFactory);
+        return template;
+    }
+}
